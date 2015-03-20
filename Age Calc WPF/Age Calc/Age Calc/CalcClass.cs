d5e5c28e1f9118aa078399e11e4ac78196451f77
Shawon Ashraf <shawon13@live.com>
2015-03-20 19:31:38 +0600

    Derp!!!!!!!

---
+++ Age Calc WPF/Age Calc/Age Calc/CalcClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows; // for messagebox
+
+namespace Age_Calc
+{
+    class CalcClass
+    {
+        private DateTime dateTime1 { get; set; }
+        private DateTime dateTime2 { get; set; }
+
+        public CalcClass(string date1, string date2)
+        {
+            dateTime1 = Convert.ToDateTime(date1);
+            dateTime2 = Convert.ToDateTime(date2);
+        }
+
+        public string getAge()
+        {
+            string age = "";
+
+            TimeSpan ts = dateTime2.Subtract(dateTime1); // time difference
+            int t = ts.Days; // covertring to days
+            int y, m, d; // y = years, m = months, d = days
+
+            y = t / 365; 
+            t = t % 365;
+
+            m = t / 30;
+            d = t % 30;
+
+            age += y + " years " + m + " months " + d + " days";
+
+            return age;
+        }
+    }
+}
---
+++ Age Calc WPF/Age Calc/Age Calc/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Age_Calc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow :Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calcAge(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string date1 = datePick1.SelectedDate.ToString();
+                string date2 = datePick2.SelectedDate.ToString();
+
+                CalcClass c = new CalcClass(date1, date2);
+
+                string s = c.getAge();
+
+                ageBox.Text = s;
+            }
+
+            catch (Exception x)
+            {
+                MessageBoxResult r = MessageBox.Show("Please select a date!!", "Attention!!!!");
+            }
+        }
+
+        private void exitApp(object sender, RoutedEventArgs e)
+        {
+            if(MessageBox.Show("Do you wish to quit?", "Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+
+            else
+            {
+                // do nothing
+                // let the application carry on
+            }
+        }
+    }
+}
